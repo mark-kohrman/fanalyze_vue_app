c306c33062711803ebc25c57085a15ba2f9b4069
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-30 23:30:05 -0500

    adding HTML table in case of emergency

--- src/views/UserTeamsShow.vue
+++ src/views/UserTeamsShow.vue
@@ -2,16 +2,85 @@
   <div class="home">
 
     <h1> {{ user_team.user_team_name }}'s Team</h1>
-    <grid :cols="cols" :rows="rows"></grid>
 
-    <p> {{ user_team.qb  }} </p>
-    <p> {{ user_team.rb1 }} </p>
-    <p> {{ user_team.rb2  }} </p>
-    <p> {{ user_team.wr1 }} </p>
-    <p> {{ user_team.wr2  }} </p>
-    <p> {{ user_team.flex }} </p>
-    <p> {{ user_team.te }} </p>
-    <p> {{ user_team.k }} </p>
+    <table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Position</th>
+    <th>Team</th>
+    <th>Projection</th>
+    <th>Weather Proj</th>
+    <th>TOTAL PROJECTION</th>
+
+
+  </tr>
+  <tr>
+    <td>{{ user_team.qb.player_name  }}</td>
+    <td>{{ user_team.qb.position  }}</td>
+    <td>{{ user_team.qb.nfl_team  }}</td>
+    <td>{{ user_team.qb.projected_points  }}</td>
+    <td>{{ user_team.qb.weather_projected_points  }}</td>
+    <td>{{ user_team.qb.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.rb1.player_name  }}</td>
+    <td>{{ user_team.rb1.position  }}</td>
+    <td>{{ user_team.rb1.nfl_team  }}</td>
+    <td>{{ user_team.rb1.projected_points  }}</td>
+    <td>{{ user_team.rb1.weather_projected_points  }}</td>
+    <td>{{ user_team.rb1.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.rb2.player_name  }}</td>
+    <td>{{ user_team.rb2.position  }}</td>
+    <td>{{ user_team.rb2.nfl_team  }}</td>
+    <td>{{ user_team.rb2.projected_points  }}</td>
+    <td>{{ user_team.rb2.weather_projected_points  }}</td>
+    <td>{{ user_team.rb2.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.wr1.player_name  }}</td>
+    <td>{{ user_team.wr1.position  }}</td>
+    <td>{{ user_team.wr1.nfl_team  }}</td>
+    <td>{{ user_team.wr1.projected_points  }}</td>
+    <td>{{ user_team.wr1.weather_projected_points  }}</td>
+    <td>{{ user_team.wr1.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.wr2.player_name  }}</td>
+    <td>{{ user_team.wr2.position  }}</td>
+    <td>{{ user_team.wr2.nfl_team  }}</td>
+    <td>{{ user_team.wr2.projected_points  }}</td>
+    <td>{{ user_team.wr2.weather_projected_points  }}</td>
+    <td>{{ user_team.wr2.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.te.player_name  }}</td>
+    <td>{{ user_team.te.position  }}</td>
+    <td>{{ user_team.te.nfl_team  }}</td>
+    <td>{{ user_team.te.projected_points  }}</td>
+    <td>{{ user_team.te.weather_projected_points  }}</td>
+    <td>{{ user_team.te.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.flex.player_name  }}</td>
+    <td>{{ user_team.flex.position  }}</td>
+    <td>{{ user_team.flex.nfl_team  }}</td>
+    <td>{{ user_team.flex.projected_points  }}</td>
+    <td>{{ user_team.flex.weather_projected_points  }}</td>
+    <td>{{ user_team.flex.total_projected_points  }}</td>
+  </tr>
+  <tr>
+    <td>{{ user_team.k.player_name  }}</td>
+    <td>{{ user_team.k.position  }}</td>
+    <td>{{ user_team.k.nfl_team  }}</td>
+    <td>{{ user_team.k.projected_points  }}</td>
+    <td>{{ user_team.k.weather_projected_points  }}</td>
+    <td>{{ user_team.k.total_projected_points  }}</td>
+  </tr>
+</table>
+
+  
 
 
   </div>
@@ -26,17 +95,8 @@ import Grid from "gridjs-vue";
 
 export default {
   name: "UserTeams",
-  components: {
-    Grid,
-  },
   data: function () {
     return {
-      message: "Welcome to the show page! This your team",
-      cols: ["Name", "Position", "Team", "Proj", "Weather Proj", "Total Proj"],
-      rows: [
-        ["Tom Brady", "QB", "TB", "15", "2", "17"],
-        ["joe", "RB", "CHI", "32", "2", "33"],
-      ],
       user_team: {},
     };
   },
