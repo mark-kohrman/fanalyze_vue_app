3d90c0537c7f84250f28582e0e98e32dccf1cdf7
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-01 19:17:47 -0500

    adding link to index page at top of page

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/signup">Signup</router-link>  |
+      <router-link to="/user_teams/new">Create Team</router-link> |
+      <router-link to="/user_teams">My Teams</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/logout">Logout</router-link> |
-      <router-link to="/user_teams/new">Create Team</router-link> 
 
 
     </div>
